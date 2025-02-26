fee091c9fc05915b771db581c012eb4a6bb2cc3c
vinmugambi <vinmugambi17@gmail.com>
2025-02-26 09:53:23 +0300

    fix(13): grade still caluclated for invalid points

--- 13.py
+++ 13.py
@@ -1,13 +1,17 @@
 # KCSE grade calculator
 minPoints = 7
-maxpoints=84
+maxpoints = 84
 
 print("KCSE grade calculator")
 points = int(input("kCSE points: "))
 
 if points < minPoints or points > maxpoints:
-    print("\033[31m", "point must be between 7 and 84 (inclusive)")
+    print("\033[31m", "Liar! Points must be between 7 and 84 (inclusive)")
+else:
+    grade = (
+        "A"
+        if points > 80
+        else "A-" if points > 73 else "B+" if points > 66 else "Disqualified"
+    )
 
-grade = "A" if points>80 else "A-" if points>73 else "B+" if points>66 else "Disqualified"
-
-print("With", points, "points,", "your grade is", grade)
+    print("With", points, "points,", "your grade is", grade)
